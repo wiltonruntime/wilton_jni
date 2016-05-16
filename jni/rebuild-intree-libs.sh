20cc4377724ad6a2592ac2bd3404a82ff41e9d0f
alex <alex@staticlibs.net>
2016-05-16 13:39:58 +0100

    rebuild script update

--- jni/rebuild-intree-libs.sh
+++ jni/rebuild-intree-libs.sh
@@ -6,14 +6,6 @@ set -x
 ANDROID_ARMEABI_TOOLCHAIN_DIR=/home/alex/apps/android-ndk-r9d-arm-linux-androideabi-4.8/
 ANDROID_X86_TOOLCHAIN_DIR=/home/alex/apps/android-ndk-r9d-x86-linux-android-4.8/
 
-echo --- linux_amd64_gcc
-. creset
-cmake ../jni \
-    -DSTATICLIB_TOOLCHAIN=linux_amd64_gcc \
-    -DCMAKE_BUILD_TYPE=Release
-make
-cp ./bin/libwilton_jni.so ../src/main/resources/lib/linux_amd64/
-
 echo --- android_armeabi_gcc
 . creset
 cmake ../jni \
@@ -31,5 +23,13 @@ cmake ../jni \
     -DCMAKE_BUILD_TYPE=Release
 make
 cp ./bin/libwilton_jni.so ../src/main/resources/lib/x86/
+
+echo --- linux_amd64_gcc
+. creset
+cmake ../jni \
+    -DSTATICLIB_TOOLCHAIN=linux_amd64_gcc \
+    -DCMAKE_BUILD_TYPE=Release
+make
+cp ./bin/libwilton_jni.so ../src/main/resources/lib/linux_amd64/
 
 echo --- FINISH_SUCCESS
