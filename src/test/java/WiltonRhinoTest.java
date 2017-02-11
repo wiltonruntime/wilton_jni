992ce8b44aac2479ddb0be5e073a6e5e268c8ee0
alex <alex@staticlibs.net>
2017-02-11 00:45:25 +0000

    rhino fine-grained context

--- src/test/java/WiltonRhinoTest.java
+++ src/test/java/WiltonRhinoTest.java
@@ -1,13 +1,19 @@
 import net.wiltonwebtoolkit.support.rhino.WiltonContextFactory;
+import org.apache.commons.io.Charsets;
+import org.apache.commons.io.FileUtils;
+import org.junit.BeforeClass;
 import org.junit.Test;
-import org.mozilla.javascript.Context;
-import org.mozilla.javascript.ScriptableObject;
-import org.mozilla.javascript.tools.shell.Global;
+import org.mozilla.javascript.*;
+import utils.TestGateway;
 
 import java.io.*;
+import java.lang.reflect.Method;
 
+import static net.wiltonwebtoolkit.WiltonJni.LOGGING_DISABLE;
+import static org.apache.commons.io.FileUtils.readFileToString;
 import static org.apache.commons.io.IOUtils.closeQuietly;
 import static org.junit.Assert.assertEquals;
+import static utils.TestUtils.initWiltonOnce;
 
 /**
  * User: alexkasko
@@ -16,40 +22,71 @@ import static org.junit.Assert.assertEquals;
 public class WiltonRhinoTest {
     private static final File RUNTESTS_JS = new File("src/test/js/runtests.js");
 
-    @Test
-    public void dummy() {
+    public static final ScriptableObject RHINO_GLOBAL_SCOPE;
 
+    // todo: to non-test support utils
+
+    static {
+        ContextFactory.initGlobal(new WiltonContextFactory());
+        Context cx = Context.enter();
+        RHINO_GLOBAL_SCOPE = cx.initStandardObjects();
+        FunctionObject loadFunc = new FunctionObject("load", RhinoLoader.getLoadMethod(), RHINO_GLOBAL_SCOPE);
+        RHINO_GLOBAL_SCOPE.put("load", RHINO_GLOBAL_SCOPE, loadFunc);
+        RHINO_GLOBAL_SCOPE.setAttributes("load", ScriptableObject.DONTENUM);
+        Context.exit();
     }
 
-//    @Test
-    public void test() throws Exception {
-        Context cx = new WiltonContextFactory().enterContext();
-        Global gl = new Global();
-        gl.init(cx);
+    private static class RhinoLoader {
 
+        public static void load(Context cx, Scriptable thisObj, Object[] args, Function funObj) throws Exception {
+            for (Object arg : args) {
+                String file = Context.toString(arg);
+                String sourceCode = readFileToString(new File(".", file), Charsets.UTF_8);
+                Script script = cx.compileString(sourceCode, file, 1, null);
+                if (script != null) {
+                    script.exec(cx, RHINO_GLOBAL_SCOPE);
+                }
+            }
+        }
+
+        public static Method getLoadMethod() {
+            try {
+                return RhinoLoader.class.getMethod("load", Context.class, Scriptable.class, Object[].class, Function.class);
+            } catch (NoSuchMethodException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    @BeforeClass
+    public static void init() {
+        // init, no logging by default, enable it when needed
+        initWiltonOnce(new TestGateway(), LOGGING_DISABLE);
+    }
+
+    @Test
+    public void test() throws Exception {
+        Context cx = Context.enter();
         InputStream is = null;
         try {
             is = new FileInputStream(RUNTESTS_JS.getAbsolutePath());
             Reader re = new InputStreamReader(is, "UTF-8");
-            cx.evaluateReader(gl, re, RUNTESTS_JS.getName(), -1, null);
+            cx.evaluateReader(RHINO_GLOBAL_SCOPE, re, RUNTESTS_JS.getName(), -1, null);
         } catch(Exception e) {
             throw new RuntimeException(e);
         } finally {
             closeQuietly(is);
         }
-
         Context.exit();
     }
 
-//    @Test
+    @Test
     // https://github.com/mozilla/rhino/issues/153
     public void testStacktrace() throws Exception {
-        Context cx = new WiltonContextFactory().enterContext();
-        Global gl = new Global();
-        gl.init(cx);
+        Context cx = Context.enter();
         Object[] holder = new Object[1];
-        ScriptableObject.putProperty(gl, "holder", Context.javaToJS(holder, gl));
-        cx.evaluateString(gl, "try {throw new Error() } catch (e) {holder[0] = e.stack}", "some_file.js", 42, null);
+        ScriptableObject.putProperty(RHINO_GLOBAL_SCOPE, "holder", Context.javaToJS(holder, RHINO_GLOBAL_SCOPE));
+        cx.evaluateString(RHINO_GLOBAL_SCOPE, "try {throw new Error() } catch (e) {holder[0] = e.stack}", "some_file.js", 42, null);
         assertEquals("\tat some_file.js:42\n", holder[0]);
         Context.exit();
     }
